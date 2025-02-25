b9b867e146ed632c7ade1bc177d2a2b178d149cd
Yevhen S <eresid@gmail.com>
2025-02-25 16:40:19 +0100

    change backend port to 4000

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Do this Shit
+# Do this Shit - BackEnd Side
 
 Do this Shit - a very simple checklist, for personal use only. Not suitable for production. But if you like the app - I would be happy for feedback and Pull Requests.
--- backend/src/constants/global.constants.ts
+++ backend/src/constants/global.constants.ts
@@ -5,7 +5,7 @@ dotenv.config({
   path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
 });
 
-export const PORT = Number(process.env.PORT) || 3000;
+export const PORT = Number(process.env.PORT) || 4000;
 export const MONGODB_URL = process.env.MONGODB_URL || "";
 export const JWT_SECRET = process.env.JWT_SECRET || "";
 export const NODE_ENV = process.env.NODE_ENV || "development";
