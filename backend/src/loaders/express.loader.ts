5ce073c0f25c8ac17da40bcf4201d3c32524d4e8
Yevhen S <eresid@gmail.com>
2025-03-01 12:39:08 +0100

    Add cors

--- backend/src/loaders/express.loader.ts
+++ backend/src/loaders/express.loader.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
 import appRoutes from "../routes";
+import corsMiddleware from "./cors.middleware";
 
 export default function loadServer() {
   const app: Express = express();
 
+  app.use(corsMiddleware);
   app.use(express.json());
 
   appRoutes(app);
